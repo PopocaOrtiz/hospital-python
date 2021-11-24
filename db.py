2a99e7aa056d76a3380b8e075a0da47c49aaa4cf
Fernando Popoca <popoca.ortiz@gmail.com>
2021-11-23 23:09:28 -0600

    insert and updte operations for mysql helper class

--- db.py
+++ db.py
@@ -15,11 +15,27 @@ class DB:
     def get_cursor(self) -> pymysql.cursors.DictCursor:
         return self.connection.cursor()
 
-    def select(self, sql: str, params: dict = None) -> list[dict]:
-
+    def execute(self, params, sql):
         if params is None:
             params = {}
 
         cursor = self.get_cursor()
-        cursor.execute(sql, params)
+        affected_rows = cursor.execute(sql, params)
+
+        return cursor, affected_rows
+
+    def select(self, sql: str, params: dict = None) -> list[dict]:
+        cursor, _ = self.execute(params, sql)
         return cursor.fetchall()
+
+    def insert(self, sql: str, params: dict) -> int:
+        cursor, _ = self.execute(params, sql)
+        self.connection.commit()
+
+        return cursor.lastrowid
+
+    def update(self, sql: str, params: dict = None):
+        _, rows_updated = self.execute(sql, params)
+        self.connection.commit()
+
+        return rows_updated
