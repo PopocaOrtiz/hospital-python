2826312e5dc3ceaa4db32e64cb900e7dafb66872
Fernando Popoca <popoca.ortiz@gmail.com>
2021-11-23 23:10:08 -0600

    create and update user

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+from db import DB
 from query_builder_users import QueryBuilderUsers
 
 app = FastAPI()
@@ -31,6 +33,52 @@ async def get_user(user_id: int):
     return {
         'user': user
     }
+
+
+class User(BaseModel):
+    id: int
+    firstname: str
+    lastname: str
+
+
+@app.post('/users')
+async def create_user(user: User):
+
+    sql = """
+        insert into users
+        (firstName, lastName)
+        values
+        (%(firstname)s, %(lastname)s)
+    """
+
+    new_id = DB().insert(sql, dict(user))
+
+    user.id =new_id
+
+    return {
+        'user': user
+    }
+
+
+@app.put('/users/{user_id}')
+def update_user(user_id: int, user: User):
+
+    sql = """
+        update users
+        set firstname = %(firstname)s,
+            lastname = %(lastname)
+        where id = %(user_id)s
+    """
+
+    user = dict(user)
+    user['user_id'] = user_id
+
+    DB().update(sql, user)
+
+    return {
+        'user': user
+    }
+
 
 
 if __name__ == '__main__':
